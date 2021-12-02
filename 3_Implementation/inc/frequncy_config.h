2907f136dc16cda47a4679ca4e72377626600a1a
tlnsnani <94216895+tlnsnani@users.noreply.github.com>
2021-12-02 13:53:25 +0530

    Update and rename Readme.md to frequncy_config.h

---
+++ 3_Implementation/inc/frequncy_config.h
@@ -0,0 +1,16 @@
+
+#ifndef frequncy_config_H_
+#define frequncy_config_H_
+
+
+/**
+ * Macro Definitions
+ */
+#include "USER_UTILS.h"
+#define F_CPU 1000000UL 	/**< Clock Frequency of MCU is 16 MHz */
+
+#define LED_PORT (PORTB)    /**< LED Port Number */
+#define LED_PIN  (PORTB0)   /**< LED Pin number  */
+
+
+#endif 
